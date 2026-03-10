75bd6089e769e329cfff4dd0548587e68c14946a
HazDeen <maxzavalov90@gmail.com>
2026-03-10 21:08:23 +0300

    change env variables

--- backend/src/tickets/tickets.service.ts
+++ backend/src/tickets/tickets.service.ts
@@ -7,9 +7,9 @@ export class TicketsService {
 
   // 🔥 Вспомогательная функция для отправки сообщений в ТГ
   private async sendTelegramMessage(chatId: string | bigint, text: string) {
-    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const token = process.env.BOT_TOKEN;
     if (!token) {
-      console.warn('TELEGRAM_BOT_TOKEN не задан в .env!');
+      console.warn('BOT_TOKEN не задан в .env!');
       return;
     }
     
